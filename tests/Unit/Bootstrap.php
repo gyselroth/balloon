3d245804c4416dec4956d4be383e16b4f3fc4dab
Fabian Jucker <jucker@gyselroth.com>
2017-09-04 16:41:57 +0200

    bugfix: load correct Hook path (changed in commit 2c3a363)

--- tests/Unit/Bootstrap.php
+++ tests/Unit/Bootstrap.php
@@ -2,5 +2,5 @@
 use Balloon\Testsuite\Test;
 
 require_once './vendor/autoload.php';
-require_once './src/app/Delta/src/lib/Hook.php';
+require_once './src/app/Balloon.App.Delta/src/lib/Hook.php';
 require_once './tests/Unit/Test.php';
