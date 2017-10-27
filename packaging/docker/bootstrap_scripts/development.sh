05bcda1393bd8e6197f6ece6ec2c80e33144042d
Fabian Jucker <jucker@gyselroth.com>
2017-10-27 14:26:48 +0200

    add php7.1-xdebug for development mode

--- packaging/docker/bootstrap_scripts/development.sh
+++ packaging/docker/bootstrap_scripts/development.sh
@@ -3,7 +3,7 @@ echo "# INSTALL BALLOON DEVELOPMENT DEPENDENCIES"
 
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
-apt-get --no-install-recommends install -y git npm make
+apt-get --no-install-recommends install -y git npm make php7.1-xdebug
 # create node alias for nodejs binary
 update-alternatives --install /usr/bin/node node /usr/bin/nodejs 10
 npm -g install apidoc
