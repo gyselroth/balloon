031f17ddf751d611ecf2aa478f03d9c452d6a9e1
Raffael Sahli <sahli@gyselroth.com>
2018-03-16 22:33:19 +0100

    restore

---
+++ tests/Unit/Filesystem/InitNodeTest.php
@@ -0,0 +1,106 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * balloon
+ *
+ * @copyright   Copryright (c) 2012-2018 gyselroth GmbH (https://gyselroth.com)
+ * @license     GPL-3.0 https://opensource.org/licenses/GPL-3.0
+ */
+
+namespace Balloon\Testsuite\Unit\Filesystem;
+
+use Balloon\Exception;
+use Balloon\Testsuite\Unit\Test;
+use Exception\InvalidArgument;
+use MongoDB\BSON\ObjectId;
+
+/**
+ * @coversNothing
+ */
+class InitNodeTest extends Test
+{
+    protected $fs;
+
+    public function setUp()
+    {
+        $server = $this->getMockServer();
+        $this->fs = $server->getFilesystem();
+    }
+
+    public function testValid()
+    {
+        // fixture
+        $nodeData = [
+            'owner' => $this->fs->getUser()->getId(),
+            '_id' => new ObjectId(),
+            'directory' => true,
+        ];
+
+        // execute SUT
+        $node = $this->fs->initNode($nodeData);
+
+        // assertion
+        $this->assertSame($node->getRawAttributes(), $nodeData);
+    }
+
+    public function testNoDirectoryAttribute()
+    {
+        $this->expectException(Exception::class);
+        // fixture
+        $nodeData = [
+            'owner' => $this->fs->getUser()->getId(),
+            '_id' => new ObjectId(),
+        ];
+
+        // execute SUT
+        $node = $this->fs->initNode($nodeData);
+    }
+
+    public function testValidParent()
+    {
+        // fixture
+        $nodeData = [
+            'owner' => $this->fs->getUser()->getId(),
+            '_id' => new ObjectId(),
+            'directory' => false,
+            'parent' => $this->fs->getRoot()->getId(),
+        ];
+
+        // execute SUT
+        $node = $this->fs->initNode($nodeData);
+
+        // assertion
+        $this->assertSame($node->getRawAttributes(), $nodeData);
+    }
+
+    /*public function testInexistentParent()
+    {
+        $this->expectException(InvalidArgument::class);
+        // fixture
+        $nodeData = [
+            'owner' => $this->fs->getUser()->getId(),
+            '_id' => new ObjectId(),
+            'directory' => false,
+            'parent' => new ObjectId(),
+        ];
+        // execute SUT
+        $node = $this->fs->initNode($nodeData);
+    }
+
+    public function testInvalidParentId()
+    {
+        $this->expectException(InvalidArgument::class);
+        // fixture
+        $nodeData = [
+            'owner' => $this->fs->getUser()->getId(),
+            '_id' => new ObjectId(),
+            'directory' => false,
+            'parent' => false,
+        ];
+
+        // execute SUT
+        $node = $this->fs->initNode($nodeData);
+    }*/
+}
