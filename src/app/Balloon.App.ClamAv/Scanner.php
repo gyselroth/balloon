c4af783f63bf3ab196cdc803cef5bdc6a4d34bbd
daniel.kleeb <kleeb@gyselroth.com>
2021-10-08 16:08:40 +0200

    Job Balloon\Async\CleanTrash ends with an error fixes #463

--- src/app/Balloon.App.ClamAv/Scanner.php
+++ src/app/Balloon.App.ClamAv/Scanner.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 /**
  * balloon
  *
- * @copyright   Copryright (c) 2012-2019 gyselroth GmbH (https://gyselroth.com)
+ * @copyright   Copyright (c) 2012-2019 gyselroth GmbH (https://gyselroth.com)
  * @license     GPL-3.0 https://opensource.org/licenses/GPL-3.0
  */
 
@@ -63,7 +63,7 @@ class Scanner
      */
     protected $logger;
 
-    /*
+    /**
      * Socket factory
      *
      * @var SocketFactory
@@ -115,7 +115,6 @@ class Scanner
 
                     break;
 
-                break;
                 default:
                     throw new InvalidArgumentException('invalid option '.$option.' given');
             }
